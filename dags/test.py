a4191082b70914e9332a2a2f0dccbb7491e6910a
July de Lima <julydelima@julys-mbp.kpn>
2022-03-28 09:39:25 +0200

    delete some files

--- dags/test.py
+++
@@ -1,7 +0,0 @@
-import time
-import datetime
-
-def convert(str_date):
-    return int(time.mktime(datetime.datetime.strptime(str_date,'%m/%d/%Y %H:%M:%S').timetuple()))
-
-print(convert('04/14/2021 13:00:00'))
